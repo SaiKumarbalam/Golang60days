9a318d976687a87670eaed8d04dea2bb8beec4e1
Sai kumar Balam <saikumarbalam@Sais-Air.attlocal.net>
2023-04-12 00:29:53 -0500

    GoLang Code Files

---
+++ 05Operators/Arthimetric_Operators/program.Subtraction.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 6
+	num2 := 5
+
+	sub := num1 - num2
+
+	fmt.Println("Subtraction:", sub)
+}
---
+++ 05Operators/Arthimetric_Operators/program_Addition.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 6
+	num2 := 5
+
+	sum := num1 + num2
+
+	fmt.Println("Addition:", sum)
+}
