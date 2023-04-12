d812acce8b02d22b33c2ec3f72f10d1617d8f417
Sai kumar Balam <saikumarbalam@Sais-Air.attlocal.net>
2023-04-12 00:32:44 -0500

    GoLang Code Files

---
+++ 04Comments/Program_multi_line_comment.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*Constants are the fixed values that cannot be changed once declared.
+	In Go, we use the const keyword to create constant variables.*/
+	const lightSpeed = 256366
+	fmt.Println(lightSpeed)
+
+}
